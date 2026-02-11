f31a6ada11d6c5c0f54fb035b78533c88dd6c3c3
Benjamin Bell <bsbell21@gmail.com>
2026-02-11 08:31:44 -0800

    updating ai agent

--- PROJECTS/AI-AGENT/2_finance_agent_llama.py
+++ PROJECTS/AI-AGENT/2_finance_agent_llama.py
@@ -1,9 +1,9 @@
 """Run `pip install yfinance` to install dependencies."""
 
-from dotenv import load_dotenv
 from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -34,12 +34,12 @@ agent = Agent(
     tools=[
         YFinanceTools(
             stock_price=True, analyst_recommendations=True, stock_fundamentals=True
-        ),
-        get_company_symbol,
-    ],
+            ), 
+           get_company_symbol
+           ],
     instructions=[
         "Use tables to display data.",
-        "If you need to find the symbol for a company, use the get_company_symbol tool.",
+        "If the symbol for a company named in the prompt is not given, always use the get_company_symbol tool to get it."
     ],
     show_tool_calls=True,
     markdown=True,
@@ -47,6 +47,6 @@ agent = Agent(
 )
 
 agent.print_response(
-    "Summarize and compare analyst recommendations and fundamentals for Tesla and Microsoft. Show in tables.",
-    stream=True,
+    "Summarize and compare analyst recommendations and fundamentals for Tesla and Microsoft. Show in tables.", 
+    stream=True
 )
